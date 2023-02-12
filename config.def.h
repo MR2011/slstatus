a904b60067ceb85b48788e3fe721030cc58b9c51
Michael <mr201190@gmail.com>
2023-02-12 13:40:58 +0100

    fix

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const struct arg args[] = {
 	{ netspeed_tx, "ﯴ %8s ",    "enp0s31f6" },
 	{ cpu_perc, " %2s%% ",     NULL },
 	{ ram_perc, " %2s%% ",     NULL },
-	{ run_command, " %s ",     "amixer sget Master | grep 'Right:' | awk -F'[][]'
+	{ run_command, " %s ",     "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'"}, 
 	{ datetime, "%s ",          " %a, %b %d %H:%M" },
 
 };
